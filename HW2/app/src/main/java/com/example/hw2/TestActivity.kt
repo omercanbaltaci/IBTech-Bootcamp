087a3dddfff0cfad78774bed31882809d353c0a6
Ömer Can Baltacı <omercanbaltaci905@gmail.com>
2021-09-11 11:08:51 +0300

    Use replace for pushing fragments instead of add

--- HW2/app/src/main/java/com/example/hw2/TestActivity.kt
+++ HW2/app/src/main/java/com/example/hw2/TestActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hw2
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.example.hw2.util.inTransaction
@@ -12,7 +13,7 @@ class TestActivity : AppCompatActivity() {
 
         // An extension function to push a fragment have been used here
         supportFragmentManager.inTransaction {
-            add(R.id.flFragment, TestFragment())
+            replace(R.id.flFragment, TestFragment())
         }
     }
 }
